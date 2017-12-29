faeb3c903145d0b2e861c1b3885a8e967a17dd15
Deepty <deeptymayi.patel@gmail.com>
2017-12-28 22:21:35 -0800

    further cleanup

--- lib/pages/android_click.rb
+++
@@ -1,6 +0,0 @@
-
-require 'watir'
-
-driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :iphone, :orientation => :landscape)
-browser = Watir::Browser.new driver, :switches => %w[--proxy-server=#{proxy.host}:#{proxy.port}]
-browser.goto 'google.com'
